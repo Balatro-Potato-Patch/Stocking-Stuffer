99de3b1ae78217ddadf198c8426a6d328f328010
BarrierTrio <vinylscratch270@gmail.com>
2025-12-12 12:06:11 -0700

    tonycommander

--- content/gote.lua
+++ content/gote.lua
@@ -14,6 +14,24 @@ StockingStuffer.Developer({
     colour = HEX('F38AC7')
 })
 
+archyarrayi = {
+    "Boss Blind",
+    "Boss Blind",
+    "Boss Blind",
+    "Boss Blind",
+    "Boss Blind",
+    "Blind"
+}
+
+archyarrayii = {
+    "",
+    "Enhanced ",
+    "Sealed ",
+    "Editioned ",
+    "Sealed and Editioned ",
+    "Sealed and Editioned ",
+}
+
 lookupcard = {
     c_empress = "c_heirophant",
     c_heirophant = "c_empress",
@@ -31,55 +49,34 @@ reverseTarot = function(card)
     else return false
     end
 end
--- Wrapped Present Template
--- key defaults to 'display_name_stocking_present'
+
 StockingStuffer.WrappedPresent({
-    developer = display_name, -- DO NOT CHANGE
+    developer = display_name,
 
-    pos = { x = 0, y = 0 }, -- position of present sprite on your atlas
-    atlas = 'gote_presents',
-    -- atlas defaults to 'stocking_display_name_presents' as created earlier but can be overriden
-
-    -- Your present will be given an automatically generated name and description. If you want to customise it you can, though we recommend keeping the {V:1} in the name
-    -- You are encouraged to use the localization file for your name and description, this is here as an example
-    -- loc_txt = {
-    --     name = '{V:1}Present',
-    --     text = {
-    --         '  {C:inactive}What could be inside?  ',
-    --         '{C:inactive}Open me to find out!'
-    --     }
-    -- },
-})
-
--- Present Template - Replace 'template' with your name
--- Note: You should make up to 5 Presents to fill your Wrapped Present!
-StockingStuffer.Present({
-    developer = display_name, -- DO NOT CHANGE
-
-    key = 'archy', -- keys are prefixed with 'display_name_stocking_' for reference
-    -- You are encouraged to use the localization file for your name and description, this is here as an example
-    -- loc_txt = {
-    --     name = 'Example Present',
-    --     text = {
-    --         'Does nothing'
-    --     }
-    -- },
     pos = { x = 0, y = 0 },
     atlas = 'gote_presents',
-    -- atlas defaults to 'stocking_display_name_presents' as created earlier but can be overriden
+})
 
+StockingStuffer.Present({
+    developer = display_name,
 
-    -- use and can_use are completely optional, delete if you do not need your present to be usable
-    can_use = function(self, card)
-        -- check for use condition here
-        return true
+    key = 'archy',
+
+    pos = { x = 0, y = 0 },
+    atlas = 'gote_presents',
+    config = {
+        state = 1,
+        blind = archyarrayi[state],
+        level = archyarrayii[state]
+    },
+
+    loc_vars = function(self, info_queue, card)
+        return {key = card.ability.state ~= 6 and "BarrierTrio/Gote_stocking_archy_A" or "BarrierTrio/Gote_stocking_archy_B", vars = {
+            card.ability.blind, card.ability.level
+        }}
     end,
-    use = function(self, card, area, copier) 
-        -- do stuff here
-        print('example')
-    end,
-    keep_on_use = function(self, card)
-        -- return true when card should be kept
+    load = function(self, card, card_table, other_card)
+        card.loaded = true
     end,
 
     -- calculate is completely optional, delete if your present does not need it
@@ -96,32 +93,18 @@ StockingStuffer.Present({
 })
 
 StockingStuffer.Present({
-    developer = display_name, -- DO NOT CHANGE
+    developer = display_name,
 
-    key = 'love', -- keys are prefixed with 'display_name_stocking_' for reference
-    -- You are encouraged to use the localization file for your name and description, this is here as an example
-    -- loc_txt = {
-    --     name = 'Example Present',
-    --     text = {
-    --         'Does nothing'
-    --     }
-    -- },
+    key = 'love',
+    
     pos = { x = 0, y = 0 },
     atlas = 'gote_presents',
-    -- atlas defaults to 'stocking_display_name_presents' as created earlier but can be overriden
+    config = { chance = 8 },
 
-
-    -- use and can_use are completely optional, delete if you do not need your present to be usable
-    can_use = function(self, card)
-        -- check for use condition here
-        return true
-    end,
-    use = function(self, card, area, copier) 
-        -- do stuff here
-        print('example')
-    end,
-    keep_on_use = function(self, card)
-        -- return true when card should be kept
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {SMODS.get_probability_vars(card, 1, card.ability.chance, 'BarrierTrio/Gote_stocking_love'), card.ability.chance},
+        }
     end,
 
     -- calculate is completely optional, delete if your present does not need it
@@ -138,35 +121,23 @@ StockingStuffer.Present({
 })
 
 StockingStuffer.Present({
-    developer = display_name, -- DO NOT CHANGE
+    developer = display_name,
 
-    key = 'hate', -- keys are prefixed with 'display_name_stocking_' for reference
-    -- You are encouraged to use the localization file for your name and description, this is here as an example
-    -- loc_txt = {
-    --     name = 'Example Present',
-    --     text = {
-    --         'Does nothing'
-    --     }
-    -- },
-    pos = { x = 0, y = 0 },
+    key = 'hate',
+    
     atlas = 'gote_presents',
-    -- atlas defaults to 'stocking_display_name_presents' as created earlier but can be overriden
+    config = { chance = 5 },
+
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {SMODS.get_probability_vars(card, 1, card.ability.chance, 'BarrierTrio/Gote_stocking_hate'), card.ability.chance},
+        }
+    end,
+    
     in_pool = function(context, self, card) 
         return false
     end,
     no_collection = true,
-    -- use and can_use are completely optional, delete if you do not need your present to be usable
-    can_use = function(self, card)
-        -- check for use condition here
-        return true
-    end,
-    use = function(self, card, area, copier) 
-        -- do stuff here
-        print('example')
-    end,
-    keep_on_use = function(self, card)
-        -- return true when card should be kept
-    end,
 
     -- calculate is completely optional, delete if your present does not need it
     calculate = function(self, card, context)
@@ -182,9 +153,9 @@ StockingStuffer.Present({
 })
 
 StockingStuffer.Present({
-    developer = display_name, -- DO NOT CHANGE
+    developer = display_name,
 
-    key = 'maggie', -- keys are prefixed with 'display_name_stocking_' for reference
+    key = 'maggie',
     pos = { x = 0, y = 0 },
     atlas = 'gote_presents',
     config = { chance = 4 },
@@ -196,9 +167,6 @@ StockingStuffer.Present({
     end,
 
     calculate = function(self, card, context)
-        -- check context and return appropriate values
-        -- StockingStuffer.first_calculation is true before jokers are calculated
-        -- StockingStuffer.second_calculation is true after jokers are calculated
         if card.debuff or #G.consumeables.cards + G.GAME.consumeable_buffer >= G.consumeables.config.card_limit then return end
         
         if context.using_consumeable and StockingStuffer.first_calculation and reverseTarot(context.consumeable.config.center.key) ~= false and	SMODS.pseudorandom_probability(card, 'BarrierTrio/Gote_stocking_maggie', 1, card.ability.chance) then
@@ -224,85 +192,61 @@ StockingStuffer.Present({
 })
 
 StockingStuffer.Present({
-    developer = display_name, -- DO NOT CHANGE
+    developer = display_name,
 
-    key = 'commander', -- keys are prefixed with 'display_name_stocking_' for reference
-    -- You are encouraged to use the localization file for your name and description, this is here as an example
-    -- loc_txt = {
-    --     name = 'Example Present',
-    --     text = {
-    --         'Does nothing'
-    --     }
-    -- },
+    key = 'commander',
     pos = { x = 0, y = 0 },
     atlas = 'gote_presents',
-    -- atlas defaults to 'stocking_display_name_presents' as created earlier but can be overriden
-
-
-    -- use and can_use are completely optional, delete if you do not need your present to be usable
+    
     can_use = function(self, card)
-        -- check for use condition here
+        
         return true
     end,
     use = function(self, card, area, copier) 
-        -- do stuff here
-        print('example')
+        local pcount = #G.stocking_present.cards
+        pcountold = pcount
+        while pcount > 0 do
+            if G.stocking_present.cards[pcount].key ~= "BarrierTrio/Gote_stocking_commander" then
+                G.stocking_present.cards[pcount]:remove()
+            end
+            pcount = pcount-1
+        end
+
+        for i=1, (pcountold+1) do
+            SMODS.add_card({area = G.stocking_present, set = 'stocking_present', key = key})
+        end
+        card_eval_status_text(card, 'extra', nil, nil, nil, {message = localize('rs_success')})
     end,
     keep_on_use = function(self, card)
-        -- return true when card should be kept
-    end,
-
-    -- calculate is completely optional, delete if your present does not need it
-    calculate = function(self, card, context)
-        -- check context and return appropriate values
-        -- StockingStuffer.first_calculation is true before jokers are calculated
-        -- StockingStuffer.second_calculation is true after jokers are calculated
-        if context.joker_main then
-            return {
-                message = 'example'
-            }
-        end
+        return false
     end
 })
 
 StockingStuffer.Present({
-    developer = display_name, -- DO NOT CHANGE
+    developer = display_name,
 
-    key = 'tony', -- keys are prefixed with 'display_name_stocking_' for reference
-    -- You are encouraged to use the localization file for your name and description, this is here as an example
-    -- loc_txt = {
-    --     name = 'Example Present',
-    --     text = {
-    --         'Does nothing'
-    --     }
-    -- },
+    key = 'tony',
     pos = { x = 0, y = 0 },
     atlas = 'gote_presents',
-    -- atlas defaults to 'stocking_display_name_presents' as created earlier but can be overriden
+    config = { used = 0 },
 
-
-    -- use and can_use are completely optional, delete if you do not need your present to be usable
-    can_use = function(self, card)
-        -- check for use condition here
-        return true
-    end,
-    use = function(self, card, area, copier) 
-        -- do stuff here
-        print('example')
-    end,
-    keep_on_use = function(self, card)
-        -- return true when card should be kept
-    end,
-
-    -- calculate is completely optional, delete if your present does not need it
     calculate = function(self, card, context)
-        -- check context and return appropriate values
-        -- StockingStuffer.first_calculation is true before jokers are calculated
-        -- StockingStuffer.second_calculation is true after jokers are calculated
-        if context.joker_main then
+        
+        if context.setting_blind then
+            card.ability.used = 0
+        end
+
+        if card.debuff then
+            return
+        end
+        
+        if StockingStuffer.first_calculation and context.before and G.GAME.hands[context.scoring_name] and G.GAME.hands[context.scoring_name].played_this_round > 1 and card.ability.used == 0 then
+            card.ability.used = 1
             return {
-                message = 'example'
-            }
+				card = card,
+				level_up = true,
+				message = localize('k_level_up_ex')
+			}
         end
     end
 })
--- localization/en-us/gote.lua
+++ localization/en-us/gote.lua
@@ -1,32 +1,48 @@
 return {
     misc = {
         dictionary = {
-            mcr_success = "Extracted!"
+            mcr_success = "Extracted!",
+            rs_success = "Switched!"
         }
     },
     descriptions = {
         stocking_present = {
-            ["BarrierTrio/Gote_stocking_archy"] = {
+            ["BarrierTrio/Gote_stocking_archy_A"] = {
                 name = 'Questing Goat Figurine',
                 text = {
-                    '{C:mult}+#1#{} Mult',
-                    '{C:inactive,s:0.8}Sword accessory included!'
+                    'When a {C:attention}#1#{} is selected, add a',
+                    'permanent {C:attention}#2#2{} of {C:hearts}Hearts{} to your hand,',
+                    'then {C:planet,E:1}upgrade{} this effect',
+                    '{stocking}before{}',
+                    '{C:inactive,s:0.8}Anthropos sword accessory included!'
+                }
+            },
+            ["BarrierTrio/Gote_stocking_archy_B"] = {
+                name = 'Questing Goat Figurine',
+                text = {
+                    'When a {C:attention}#1#{} is selected, add a',
+                    'permanent {C:attention}#2#2{} of {C:hearts}Hearts{} to your hand',
+                    '{stocking}before{}',
+                    '{C:inactive,s:0.8}This is even its final form!'
                 }
             },
             ["BarrierTrio/Gote_stocking_love"] = {
                 name = 'Lovely Tiger Figurine',
                 text = {
-                    'Played {C:attention}cards',
-                    'permanently gain',
-                    '{C:chips}+#1#{} Chips when scored',
-                    'in a {C:attention}Straight',
+                    'Played cards with {C:hearts}Heart{} suit',
+                    'have a {C:green}#1# in #2#{} chance',
+                    'to upgrade played {C:attention}poker hand',
+                    '{stocking}before{}',
                     '{C:inactive,s:0.8}With real {}{C:spades}Spades{}{C:inactive,s:0.8} swapping action!'
                 }
             },
             ["BarrierTrio/Gote_stocking_hate"] = {
                 name = 'Alternate Tiger Figurine',
                 text = {
-                    'Someone has been bad',
+                    'Played cards with {C:spades}Spade{} suit',
+                    'have a {C:green}#1# in #2#{} chance',
+                    'to be {C:attention}destroyed',
+                    '{stocking}after{}',
                     "{C:inactive,s:0.8}With real {}{C:hearts}Hearts{}{C:inactive,s:0.8} swapping action!"
                 }
             },
@@ -36,14 +52,15 @@ return {
                     '{C:green}#1# in #2#{} chance after using an',
                     '{C:attention}Enhancement {C:tarot}Tarot{} card to create',
                     'the {C:attention}opposite{} card',
+                    '{C:inactive}(Must have room)',
                     '{C:inactive,s:0.8}Rustbucket RV sold seperately.'
                 }
             },
             ["BarrierTrio/Gote_stocking_commander"] = {
                 name = 'Commanding Figurine',
                 text = {
-                    '{C:green}Reroll{} the {C:attention}entire{} shop for {C:money}free',
-                    '{C:inactive}Can only be used once per shop',
+                    '{C:eternal}Use{} to replace all {C:green}Presents{} with',
+                    'random {C:green}Presents{}, including this one',
                     '{stocking}usable{}',
                     '{C:inactive,s:0.8}Feel the POWER!',
                     '{C:inactive,s:0.8}(Yes, you opened it. He just looks like that.)'
@@ -52,8 +69,10 @@ return {
             ["BarrierTrio/Gote_stocking_tony"] = {
                 name = 'Tony, from Streetlight',
                 text = {
-                    '{C:green}Reroll{} the {C:attention}entire{} shop for {C:money}free',
-                    '{C:inactive}Can only be used once per shop',
+                    'Once per {C:attention}Blind{}, upgrade level',
+                    'of played {C:attention}poker hand{} if it was',
+                    'previously played this round',
+                    '{stocking}before{}',
                     '{C:inactive,s:0.8}tony from streetlight'
                 }
             }
