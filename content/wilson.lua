17848d4b0ed984780452929916346606c8e03271
WilsontheWolf <git@shorty.systems>
2025-12-14 23:44:22 -0700

    WilsontheWolf's Stocking Stuffer (#58)

---
+++ content/wilson.lua
@@ -0,0 +1,278 @@
+local display_name = 'WilsontheWolf'
+local short_name = "wilson"
+
+SMODS.Atlas({
+    key = display_name..'_presents',
+    path = short_name .. '_presents.png',
+    px = 71,
+    py = 95
+})
+
+local colour = SMODS.Gradient{
+    key = "wilson_colour",
+    cycle = 60,
+    colours = {
+        G.C.GOLD,
+        lighten(G.C.PURPLE, 0.2),
+    }
+}
+
+colour:inject() -- For some reason it doesn't get injected soon enough and as such has no default values
+
+StockingStuffer.Developer({
+    name = display_name,
+    colour = colour,
+})
+
+StockingStuffer.WrappedPresent({
+    developer = display_name,
+    pos = { x = 0, y = 0 },
+})
+
+StockingStuffer.Present({
+    developer = display_name,
+
+    key = 'walkman',
+    pos = { x = 1, y = 0 },
+    config = { extra = { mult = 0, mult_mod = 2 } },
+
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = { card.ability.extra.mult_mod, card.ability.extra.mult },
+        }
+    end,
+
+    calculate = function(self, card, context)
+        if not StockingStuffer.first_calculation then return end
+
+        if context.before and not context.blueprint then
+            local diff = #context.full_hand - #context.scoring_hand
+            if diff > 0 then
+                local extra = card.ability.extra
+                extra.mult = extra.mult + extra.mult_mod * diff
+
+                return {
+                    message = localize('k_upgrade_ex'),
+                    colour = G.C.CHIPS,
+                }
+            end
+        end
+
+        if context.joker_main then
+            return {
+                chips = card.ability.extra.mult,
+            }
+        end
+    end
+})
+
+StockingStuffer.Present({
+    developer = display_name,
+
+    key = 'flash_drive',
+    pos = { x = 2, y = 0 },
+    config = {},
+    -- Adjusts the hitbox on the item
+    pixel_size = { w = 50, h = 53 },
+
+    set_ability = function (self, card)
+        if not card.ability.extra then
+            card.ability.extra = 2^pseudorandom('stocking_wilson_flash_drive', 3, 5)
+        end
+    end,
+
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = { card.ability.extra },
+        }
+    end,
+
+    calculate = function(self, card, context)
+       local mod = StockingStuffer.second_calculation and -1 or 1
+
+        if context.joker_main then
+            return {
+                mult = card.ability.extra * mod,
+            }
+        end
+    end
+})
+
+StockingStuffer.Present({
+    developer = display_name,
+
+    key = 'loose_wires',
+    pos = { x = 3, y = 0 },
+    config = { extra = { odds = 2, money = 5 } },
+    -- Adjusts the hitbox on the item
+    pixel_size = { w = 55, h = 77 },
+
+    set_ability = function (self, card)
+        if not card.ability.extra.tag then
+            card.ability.extra.tag = get_next_tag_key()
+        end
+    end,
+
+    loc_vars = function(self, info_queue, card)
+        local collection = card.area.config.collection
+        if not collection then
+            info_queue[#info_queue+1] = G.P_TAGS[card.ability.extra.tag]
+        end
+        local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'stocking_wilson_wires')
+        return {
+            vars = {
+                numerator,
+                denominator,
+                card.ability.extra.money,
+                collection and "[" .. localize("wilson_tag_random") .. "]" or localize{type = "name_text", set = "Tag", key = card.ability.extra.tag},
+            },
+
+        }
+    end,
+
+    calculate = function(self, card, context)
+        if not StockingStuffer.second_calculation then return end
+
+        if context.end_of_round and context.game_over == false and context.main_eval and not context.blueprint then
+            if SMODS.pseudorandom_probability(card, 'stocking_wilson_wires', 1, card.ability.extra.odds) then
+                SMODS.destroy_cards(card)
+                return {
+                    message = localize('wilson_lost')
+                }
+            else
+                return {
+                    message = localize('k_safe_ex')
+                }
+            end
+        end
+    end,
+
+    use = function(self, card)
+        if SMODS.pseudorandom_probability(card, 'stocking_wilson_wires', 1, card.ability.extra.odds) then
+            SMODS.calculate_effect({message = localize('wilson_success')}, card)
+            add_tag(Tag(card.ability.extra.tag, false, 'Big'))
+            -- TODO: Investiage activating tags instantly
+        else
+            SMODS.calculate_effect({ dollars = -card.ability.extra.money }, card)
+        end
+    end,
+
+    can_use = function(self, card)
+        return (G.GAME.dollars-G.GAME.bankrupt_at) - card.ability.extra.money >= 0
+    end
+})
+
+local function phoneRandom(card)
+    card.ability.extra.state = pseudorandom('stocking_wilson_phone', 0, 5)
+end
+
+StockingStuffer.Present({
+    developer = display_name,
+
+    key = 'phone',
+    pos = { x = 4, y = 0 },
+    config = { extra = { num = 9, state = 0 } },
+    -- Adjusts the hitbox on the item
+    pixel_size = { w = 61, h = 75 },
+
+    set_ability = function (self, card)
+        phoneRandom(card)
+    end,
+
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = { card.ability.extra.num },
+            key = self.key .. "_" .. card.ability.extra.state
+        }
+    end,
+
+    calculate = function(self, card, context)
+        local state = card.ability.extra.state
+        local num = card.ability.extra.num
+
+        if StockingStuffer.second_calculation then
+            if context.end_of_round and context.game_over == false and context.main_eval and not context.blueprint then
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        phoneRandom(card)
+                        return true
+                    end
+                }))
+                return {
+                    message = localize('wilson_ring'),
+                }
+            end
+
+            if state == 1 and context.joker_main then
+                return {
+                    chips = num
+                }
+            end
+
+            if state == 4 and context.repetition and context.cardarea == G.play and context.other_card:get_id() == num then
+                return {
+                    repetitions = 1,
+                }
+            end
+
+            if state == 5 and context.individual and context.cardarea == G.play and context.other_card:get_id() == num then
+                return {
+                    mult = num,
+                }
+            end
+
+        elseif StockingStuffer.first_calculation then
+            if state == 0 and context.joker_main then
+                return {
+                    mult = num,
+                }
+            end
+
+            if state == 2 and context.end_of_round and context.game_over == false and context.main_eval then
+                return {
+                    dollars = num,
+                }
+            end
+
+            if state == 3 and context.individual and context.cardarea == G.play and context.other_card:get_id() == num then
+                return {
+                    chips = num,
+                }
+            end
+        end
+    end
+})
+
+StockingStuffer.Present({
+    developer = display_name,
+
+    key = 'walkie',
+    pos = { x = 5, y = 0 },
+    config = { extra = 15 },
+    -- Adjusts the hitbox on the item
+    pixel_size = { w = 40, h = 77 },
+
+    loc_vars = function(self, info_queue, card)
+        info_queue[#info_queue+1] = G.P_TAGS.tag_double
+        return {
+            vars = {
+                localize{type = "name_text", set = "Tag", key = "tag_double" },
+                card.ability.extra
+            },
+        }
+    end,
+
+    use = function(self, card)
+        SMODS.calculate_effect({message = localize('wilson_copy')}, card)
+        ease_dollars(-card.ability.extra)
+        add_tag(Tag("tag_double"))
+    end,
+
+    can_use = function(self, card)
+        return (G.GAME.dollars-G.GAME.bankrupt_at) - card.ability.extra >= 0
+    end,
+
+    keep_on_use = function(self, card) return true end,
+    disable_use_animation = true,
+})
+
---
+++ localization/en-us/wilson.lua
@@ -0,0 +1,144 @@
+local phoneName = "Rotary Phone"
+local phoneShared = {
+    "Ability changes",
+    "at end of round",
+    '{stocking}after{}',
+}
+
+return {
+    misc = {
+        dictionary = {
+            wilson_tag_random = "Random Tag",
+            wilson_lost = "Lost",
+            wilson_success = "Success",
+            wilson_ring = "Ring!",
+            wilson_copy = "Copy That!",
+        }
+    },
+    descriptions = {
+        stocking_wrapped_present = {
+            WilsontheWolf_stocking_present = {
+                name = '{V:1}Present',
+                text = {
+                    '{C:inactive}Sounds like something',
+                    '{C:inactive}is just loose inside'
+                }
+            },
+        },
+        stocking_present = {
+            WilsontheWolf_stocking_walkman = {
+                name = 'Walkman',
+                text = {
+                    'This Present gains {C:chips}+#1#{} Chips',
+                    'for each {C:attention}played{} card',
+                    'that {C:attention}does not{} score',
+                    '{C:inactive}(Currently {C:chips}+#2#{C:inactive} Chips)',
+                    '{stocking}before{}',
+                }
+            },
+            WilsontheWolf_stocking_flash_drive = {
+                name = 'USB Flash Drive',
+                text = {
+                    {
+                        "{C:mult}+#1#{} Mult",
+                        '{stocking}before{}',
+                    },
+                    {
+                        "{C:mult}-#1#{} Mult",
+                        '{stocking}after{}',
+                    },
+                }
+            },
+            WilsontheWolf_stocking_loose_wires = {
+                name = 'Loose Wires',
+                text = {
+                    {
+                        "{C:green}#1# in #2#{} chance",
+                        "for a free {C:attention}#4#",
+                        "otherwise lose {C:money}$#3#",
+                        '{stocking}usable{}',
+                    },
+                    {
+                        "{C:green}#1# in #2#{} chance this Present",
+                        "is destoryed at end of round",
+                        '{stocking}after{}',
+                    },
+                }
+            },
+            WilsontheWolf_stocking_phone_0 = {
+                name = phoneName,
+                text = {
+                    {
+                        "{C:mult}+#1#{} Mult",
+                        '{stocking}before{}',
+                    },
+                    phoneShared,
+                }
+            },
+            WilsontheWolf_stocking_phone_1 = {
+                name = phoneName,
+                text = {
+                    {
+                        "{C:chips}+#1#{} Chips",
+                        '{stocking}after{}',
+                    },
+                    phoneShared,
+                }
+            },
+            WilsontheWolf_stocking_phone_2 = {
+                name = phoneName,
+                text = {
+                    {
+                        "Earn {C:money}$#1#{} at",
+                        "end of round",
+                        '{stocking}before{}',
+                    },
+                    phoneShared,
+                }
+            },
+            WilsontheWolf_stocking_phone_3 = {
+                name = phoneName,
+                text = {
+                    {
+                        "Each scored {C:attention}#1#{}",
+                        "gives {C:chips}+#1#{} Chips",
+                        '{stocking}before{}',
+                    },
+                    phoneShared,
+                }
+            },
+            WilsontheWolf_stocking_phone_4 = {
+                name = phoneName,
+                text = {
+                    {
+                        "Retrigger all",
+                        "played {C:attention}#1#{} cards",
+                        '{stocking}after{}',
+                    },
+                    phoneShared,
+                }
+            },
+            WilsontheWolf_stocking_phone_5 = {
+                name = phoneName,
+                text = {
+                    {
+                        "Each scored {C:attention}#1#{}",
+                        "gives {C:mult}+#1#{} Mult",
+                        '{stocking}after{}',
+                    },
+                    phoneShared,
+                }
+            },
+            WilsontheWolf_stocking_walkie = {
+                name = '2-Way Radio',
+                text = {
+                    {
+                        "Receive a {C:attention}#1#",
+                        "for {C:money}$#2#",
+                        '{stocking}usable{}',
+                    },
+                }
+            },
+        },
+    }
+}
