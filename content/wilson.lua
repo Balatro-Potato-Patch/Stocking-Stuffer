10a14a406b79bc9530e551b9c0d1fd7bbc371567
WilsontheWolf <git@shorty.systems>
2025-12-14 20:07:19 -0700

    feat: wires + flash drive

--- content/wilson.lua
+++ content/wilson.lua
@@ -55,3 +55,98 @@ StockingStuffer.Present({
     end
 })
 
+StockingStuffer.Present({
+    developer = display_name,
+
+    key = 'flash_drive',
+    pos = { x = 2, y = 0 },
+    config = {},
+    -- Adjusts the hitbox on the item
+    pixel_size = { w = 50, h = 53 },
+
+    set_ability = function (self, card)
+        if not card.ability.extra then
+            card.ability.extra = 2^pseudorandom('stocking_wilson_flash_drive', 3, 5)
+        end
+    end,
+
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = { card.ability.extra },
+        }
+    end,
+
+    calculate = function(self, card, context)
+       local mod = StockingStuffer.second_calculation and -1 or 1
+
+        if context.joker_main then
+            return {
+                mult = card.ability.extra * mod,
+            }
+        end
+    end
+})
+
+StockingStuffer.Present({
+    developer = display_name,
+
+    key = 'loose_wires',
+    pos = { x = 3, y = 0 },
+    config = { extra = { odds = 2, money = 5 } },
+    -- Adjusts the hitbox on the item
+    pixel_size = { w = 55, h = 77 },
+
+    set_ability = function (self, card)
+        if not card.ability.extra.tag then
+            card.ability.extra.tag = get_next_tag_key()
+        end
+    end,
+
+    loc_vars = function(self, info_queue, card)
+        local collection = card.area.config.collection
+        if not collection then
+            info_queue[#info_queue+1] = G.P_TAGS[card.ability.extra.tag]
+        end
+        local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'stocking_wilson_wires')
+        return {
+            vars = {
+                numerator,
+                denominator,
+                card.ability.extra.money,
+                collection and "[" .. localize("wilson_tag_random") .. "]" or localize{type = "name_text", set = "Tag", key = card.ability.extra.tag},
+            },
+
+        }
+    end,
+
+    calculate = function(self, card, context)
+        if not StockingStuffer.second_calculation then return end
+
+        if context.end_of_round and context.game_over == false and context.main_eval and not context.blueprint then
+            if SMODS.pseudorandom_probability(card, 'stocking_wilson_wires', 1, card.ability.extra.odds) then
+                SMODS.destroy_cards(card)
+                return {
+                    message = localize('wilson_lost')
+                }
+            else
+                return {
+                    message = localize('k_safe_ex')
+                }
+            end
+        end
+    end,
+
+    use = function(self, card)
+        if SMODS.pseudorandom_probability(card, 'stocking_wilson_wires', 1, card.ability.extra.odds) then
+            SMODS.calculate_effect({message = localize('wilson_success')}, card)
+            add_tag(Tag(card.ability.extra.tag, false, 'Big'))
+            -- TODO: Investiage activating tags instantly
+        else
+            SMODS.calculate_effect({ dollars = -card.ability.extra.money }, card)
+        end
+    end,
+
+    can_use = function(self, card)
+        return (G.GAME.dollars-G.GAME.bankrupt_at) - card.ability.extra.money >= 0
+    end
+})
--- localization/en-us/wilson.lua
+++ localization/en-us/wilson.lua
@@ -1,6 +1,9 @@
 return {
     misc = {
         dictionary = {
+            wilson_tag_random = "Random Tag",
+            wilson_lost = "Lost",
+            wilson_success = "Success",
         }
     },
     descriptions = {
@@ -13,6 +16,35 @@ return {
                     'that {C:attention}does not{} score',
                     '{C:inactive}(Currently {C:chips}+#2#{C:inactive} Chips)',
                     '{stocking}before{}',
+                }
+            },
+            WilsontheWolf_stocking_flash_drive = {
+                name = 'USB Flash Drive',
+                text = {
+                    {
+                        "{C:mult}+#1#{} Mult",
+                        '{stocking}before{}',
+                    },
+                    {
+                        "{C:mult}-#1#{} Mult",
+                        '{stocking}after{}',
+                    },
+                }
+            },
+            WilsontheWolf_stocking_loose_wires = {
+                name = 'Loose Wires',
+                text = {
+                    {
+                        "{C:green}#1# in #2#{} chance",
+                        "for a free {C:attention}#4#",
+                        "otherwise lose {C:money}$#3#",
+                        '{stocking}usable{}',
+                    },
+                    {
+                        "{C:green}#1# in #2#{} chance this Present",
+                        "is destoryed at end of round",
+                        '{stocking}after{}',
+                    },
                 }
             },
         },
