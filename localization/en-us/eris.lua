91ffdf631136fe42354cb81c320b82d4ccf18010
WilsontheWolf <git@shorty.systems>
2025-12-14 23:14:56 -0700

    Squashed commit of the following:
    commit a16e5668c06c427347f0828359926033ff2be4ef
    Merge: 6e1559d 1d85dae
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Sun Dec 14 20:16:06 2025 +0100
    
        Merge branch 'Balatro-Potato-Patch:main' into main
    
    commit 6e1559d6cd48f8fd89a1ed0fdc5c239f9283708a
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Sun Dec 14 13:51:35 2025 +0100
    
        corkscrew art
    
    commit a94847cd40642e39132d849cbea3236e75d31438
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Sun Dec 14 13:46:10 2025 +0100
    
        30 copies of balatro air riders
    
    commit d8508ce1b80b1da9a2b014f2e6a1518f919bb37b
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Sun Dec 14 13:37:19 2025 +0100
    
        prism art
    
    commit 79fbcda0cbd0fe9163f1f9993652becf0203f437
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Sun Dec 14 13:25:28 2025 +0100
    
        fix stuff
    
    commit 429ad96fe520b46f3b0fefccee4ae33951d2b140
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Sun Dec 14 13:21:19 2025 +0100
    
        i hope its a ps5
    
    commit 74a919461c5f9dcc19edcc1303e52cab510fba10
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Sun Dec 14 13:20:25 2025 +0100
    
        some art
    
    commit 7ac51e6896fa13a48bc64390a970a1af7b06f6ad
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Sun Dec 14 12:48:31 2025 +0100
    
        corkscrew
    
    commit e22661240f5a3eeed94607c46f51bbd80599c408
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Sun Dec 14 12:42:25 2025 +0100
    
        30 copies of kirby air riders
    
    commit c57e0224574d90ece6c9e13909467e37a9173557
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Sun Dec 14 12:25:56 2025 +0100
    
        prism
    
    commit 35a666991dde5b1407d419e6337abaffa308d9a0
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Sun Dec 14 12:09:32 2025 +0100
    
        1000 bananas
    
    commit ba29c09a3e3bfdb9499250c486e62a105992908b
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Sun Dec 14 12:00:54 2025 +0100
    
        evil ass bomb
    
    commit e7a6b7c7cf6c9671fe9faa19f59b623b4d07fa76
    Merge: 8eaca68 bb0c6ae
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Wed Dec 3 16:24:39 2025 +0100
    
        Merge branch 'Balatro-Potato-Patch:main' into main
    
    commit 8eaca68dddabc3499ff4d8123b0a6201bc9d1e57
    Author: Eris (borb) <remcosveen@gmail.com>
    Date:   Tue Dec 2 20:52:47 2025 +0100
    
        actually start

---
+++ localization/en-us/eris.lua
@@ -0,0 +1,74 @@
+return {
+    descriptions = {
+        stocking_present = {
+            Eris_stocking_evil_bomb = {
+                name = "Evil Bomb",
+                text = {
+                    "Every {C:attention}#1# {C:inactive}(#2#){} played hands",
+                    "{C:attention}destroys{} all played cards",
+                    "{stocking}before"
+                }
+            },
+            Eris_stocking_bananas = {
+                name = "#4# Bananas",
+                text = {
+                    {"{C:mult}+#1#{} Mult"},
+                    {
+                        "Each individual {C:mult}Mult{} has a",
+                        "{C:green}#2# in #3#{} chance to be lost",
+                        "at the end of round",
+                        "{stocking}before"
+                    }
+                }
+            },
+            Eris_stocking_prism = {
+                name = "Prism",
+                text = {
+                    {
+                        "Copies leftmost Joker",
+                        "{stocking}before"
+                    },
+                    {
+                        "Copies rightmost Joker",
+                        "{stocking}after"
+                    },
+                    {
+                        "Can only copy {C:blue}Common",
+                        "and {C:green}Uncommon {C:attention}Jokers"
+                    }
+                }
+            },
+            Eris_stocking_air_riders = {
+                name = "#1# Copies of Kirby Air Riders",
+                text = {
+                    {
+                        "{C:chips}+#2#{} Chips",
+                        "Increases by {C:chips}+#1#{} when a",
+                        "booster pack is {C:attention}skipped",
+                        "{stocking}before"
+                    },
+                    {
+                        "{C:mult}+#3#{} Mult",
+                        "Increases by {C:mult}+#1#{} when",
+                        "a blind is {C:attention}skipped",
+                        "{stocking}after"
+                    }
+                }
+            },
+            Eris_stocking_corkscrew = {
+                name = "Corkscrew",
+                text = {
+                    "{C:blue}+#1#{} Chips, then",
+                    "{C:attention}swaps{} {C:blue}Chips{} and {C:mult}Mult{}",
+                    "{C:inactive}i ran out of coal"
+                }
+            },
+            Eris_stocking_present = {
+                name = "{V:1}Present?",
+                text = {
+                    "i hope its a ps5"
+                }
+            }
+        }
+    }
+}
